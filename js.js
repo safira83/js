63f21a89b8df9f3f54303d0a047dda616716ebba
safira83 <101499861+safira83@users.noreply.github.com>
2022-04-15 17:31:40 +0700

    Update js.js

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-<script type="text/javascript">var _Hasync= _Hasync|| [];
+var _Hasync= _Hasync|| [];
 _Hasync.push(['Histats.start', '1,4627765,4,0,0,0,00010000']);
 _Hasync.push(['Histats.fasi', '1']);
 _Hasync.push(['Histats.track_hits', '']);
@@ -6,5 +6,4 @@ _Hasync.push(['Histats.track_hits', '']);
 var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
 hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-})();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4627765&101" alt="hit tracker" border="0"></a></noscript>
+})();
